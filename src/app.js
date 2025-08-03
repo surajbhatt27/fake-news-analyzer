e53cd5e0ef4440d35046571d49e0ac460d9934e8
surajbhatt27 <bhattsuraj0066@gmail.com>
2025-08-03 20:02:30 +0530

    feat: update welcome route to include uptime and environment details

--- src/app.js
+++ src/app.js
@@ -8,6 +8,13 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
+app.get("/", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Welcome to LieDetector API",
+    });
+});
+
 app.use('/api/fake-news-detect', fakeNewsRoutes);
 
 export {app}
